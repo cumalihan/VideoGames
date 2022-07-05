26976474bddb5462c188a465a989ca080db576d6
cumalihan <cmhunlu@yahoo.com>
2022-07-06 01:44:10 +0300

    Initial Commit

---
+++ VideoGames/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoGames
+//
+//  Created by Cumali Han Ünlü on 6.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
