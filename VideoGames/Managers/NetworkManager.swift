693ad6d611229d5d78d87425759102a6949528bb
cumalihan <cmhunlu@yahoo.com>
2022-07-06 14:18:01 +0300

    first commit

---
+++ VideoGames/Managers/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkManager.swift
+//  VideoGames
+//
+//  Created by Cumali Han Ünlü on 6.07.2022.
+//
+
+import Foundation
---
+++ VideoGames/TabBarController.swift
@@ -0,0 +1,38 @@
+//
+//  TabBarController.swift
+//  VideoGames
+//
+//  Created by Cumali Han Ünlü on 6.07.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemBlue
+        viewControllers = [createSearchNC(),createFavoritesNC()]
+        
+    }
+    
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = ViewController()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesNC() -> UINavigationController{
+        let favoritesNC = ViewController()
+        favoritesNC.title = "Favorites"
+        favoritesNC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesNC)
+    }
+
+    
+
+}
